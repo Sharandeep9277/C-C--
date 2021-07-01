d3a7ebe97fa6f9c218575ca88dcb8f61bfbe89d6
Shreyam <sm8967724231@gmail.com>
2021-07-01 05:53:48 +0530

    pusshhh

--- question5.c
+++ question5.c
@@ -2,7 +2,17 @@
 e = 1 + 1/1! + 1/2! + 1/3! ......
 The loop must terminate when the difference between two successive values of e is less then 0.00001 */
 #include <stdio.h>
-
 int main()
 {
+	double term = 1.0;
+	double sum = 1.0;
+	int n = 0;
+	while (term >= 0.0000001)
+	{
+		n++;
+		term = term/n;
+		sum = sum + term;
+	}
+	printf("\n Approximate value of e is: %lf ",sum);
+	return 0;
 }
